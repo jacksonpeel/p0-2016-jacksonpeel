c53b70f70db0ab74e6578a639693d6aaa6a84d51
jacksonpeel <jackson.peel@mtchs.org>
2016-09-08 08:14:49 -0600

    P0
    Completed P0

---
+++ ModernArt.java
@@ -0,0 +1,22 @@
+/*This is the main and only class
+ * It prints out modern art inspired by Piet Mondrian
+ * 
+ * @author Jackson Peel
+ * @version 1.0
+ */
+                                                                                          
+
+public class ModernArt {
+	/*
+	 * This method prints out the art
+	*/
+	public static void main (String[] args){
+		System.out.println("*******************************");
+		System.out.println("*                             *");
+		System.out.println("*******************************");
+		System.out.println("*             *               *");
+		System.out.println("*             ******************");
+		System.out.println("*             *               *");
+		System.out.println("*******************************");
+	}
+}
